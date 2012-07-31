daf9035a97d9d0d67265036ff43855a448c9d325
Tobias Kuhn <kuhntobias@gmail.com>
2012-07-30 21:43:20 -0400

    Initial commit of old code

---
+++ README.md
@@ -0,0 +1,6 @@
+AceRules
+========
+
+Copyright 2008-2012, Tobias Kuhn, http://www.tkuhn.ch
+
+AceRules is a rule engine based on Attempto Controlled English (ACE).
---
+++ engine/parameters.pl
@@ -0,0 +1,4 @@
+% This file contains parameters (actually only one so far) for AceRules.
+
+% Defines where the compiled APE package is located:
+parameter(ape_location, '../ape').
